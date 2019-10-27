64af56109b74fb5ba607b2f269f0118397b4c141
Luis Borjas <luisfborjas@gmail.com>
2019-10-27 01:32:11 -0400

    Simple stats based on text input!

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (Model, Msg(..), exampleNavbar, fontAwesomeCDN, main, view)
+module Main exposing (Model, Msg(..), countLines, countStanzas, countWords, exampleNavbar, fontAwesomeCDN, main, view)
 
 import Browser
 import Bulma.CDN exposing (..)
@@ -9,25 +9,56 @@ import Bulma.Form exposing (..)
 import Bulma.Layout exposing (..)
 import Bulma.Modifiers exposing (..)
 import Bulma.Modifiers.Typography exposing (textCentered)
-import Html exposing (Attribute, Html, a, br, i, img, input, main_, option, p, small, span, strong, text, textarea)
+import Html exposing (Attribute, Html, a, br, div, i, img, input, main_, option, p, small, span, strong, text, textarea)
 import Html.Attributes exposing (attribute, class, cols, href, placeholder, rel, rows, src, style, type_)
+import Html.Events exposing (onInput)
+import Maybe
+import Regex
 
 
-type alias Model =
-    {}
 
-
-type Msg
-    = NoOp
+-- MAIN
 
 
 main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = {}
+        { init = init
         , view = view
-        , update = \msg -> \model -> model
+        , update = update
         }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { content : String }
+
+
+init : Model
+init =
+    { content = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change newContent ->
+            { model | content = newContent }
+
+
+
+-- VIEW
 
 
 fontAwesomeCDN =
@@ -46,7 +77,7 @@ view model =
         , exampleNavbar
         , section NotSpaced
             []
-            [ container [] [ editor ] ]
+            [ container [] [ editor model ] ]
         ]
 
 
@@ -76,6 +107,55 @@ exampleNavbar =
         ]
 
 
-editor : Html Msg
-editor =
-    textarea [ rows 100, cols 120, class "textarea is-info" ] []
+stats : Model -> Html Msg
+stats model =
+    div []
+        [ span [] [ text ("Words: " ++ countWords model.content) ]
+        , span [] [ text ("Lines: " ++ countLines model.content) ]
+        , span [] [ text ("Stanzas: " ++ countStanzas model.content) ]
+        ]
+
+
+editor : Model -> Html Msg
+editor model =
+    div []
+        [ stats model
+        , textarea [ rows 100, cols 120, class "textarea is-info", onInput Change ] []
+        ]
+
+
+
+-- PURE FNS
+
+
+countWords : String -> String
+countWords s =
+    String.words s
+        |> List.length
+        |> String.fromInt
+
+
+countLines : String -> String
+countLines s =
+    String.lines s
+        |> List.length
+        |> String.fromInt
+
+
+
+-- with some help from:
+-- http://jsfiddle.net/ahRHC/2/
+-- (https://stackoverflow.com/questions/22272590/count-number-of-paragraphs-excluding-spaces)
+
+
+stanzaPattern : Regex.Regex
+stanzaPattern =
+    Maybe.withDefault Regex.never <|
+        Regex.fromString "[\\r\\n]{2,}(?!\\s*$)"
+
+
+countStanzas : String -> String
+countStanzas s =
+    Regex.split stanzaPattern s
+        |> List.length
+        |> String.fromInt
